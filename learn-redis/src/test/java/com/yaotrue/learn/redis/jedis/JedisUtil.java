eed54eac7259646603885b3eb411ca565fa245af
yaotrue <yaotrue@163.com>
2017-05-25 08:33:45 +0800

    add redis

---
+++ learn-redis/src/test/java/com/yaotrue/learn/redis/jedis/JedisUtil.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 1999-2017 YaoTrue.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yaotrue.learn.redis.jedis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @author <a href="mailto:yaotrue@163.com">yaotrue</a>
+ * 2017年5月25日 上午12:05:48
+ */
+public class JedisUtil {
+
+	 //Redis服务器IP
+    private static String ADDR = "127.0.0.1";
+    
+    //Redis的端口号
+    private static int PORT = 6379;
+    
+    //访问密码
+    private static String AUTH = "yaotrue";
+    
+    //可用连接实例的最大数目，默认值为8；
+    //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
+    private static int MaxTotal = 1024;
+    
+    //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
+    private static int MAX_IDLE = 200;
+    
+    private static int TIMEOUT = 10000;
+    
+    //等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
+    private static int MAX_WAIT = 1000 * 10;
+    
+    //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+    private static boolean TEST_ON_BORROW = true;
+    
+    private static JedisPool jedisPool = null;
+    
+    /**
+     * 初始化Redis连接池
+     */
+    static {
+        try {
+        	JedisPoolConfig config = new JedisPoolConfig();
+            // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+            config.setBlockWhenExhausted(true);
+            // 设置的逐出策略类名, 默认DefaultEvictionPolicy(当连接超过最大空闲时间,或连接数超过最大空闲连接数)
+            config.setEvictionPolicyClassName("org.apache.commons.pool2.impl.DefaultEvictionPolicy");
+            // 是否启用pool的jmx管理功能, 默认true
+            config.setJmxEnabled(true);
+            // 最大空闲连接数, 默认8个 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
+            config.setMaxIdle(MAX_IDLE);
+            // 最大连接数, 默认8个
+            config.setMaxTotal(MaxTotal);
+            // 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
+            config.setMaxWaitMillis(MAX_WAIT);
+            // 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+            config.setTestOnBorrow(TEST_ON_BORROW);
+            jedisPool = new JedisPool(config, ADDR, PORT, TIMEOUT, AUTH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * 获取Jedis实例
+     * @author <a href="mailto:yaotrue@163.com">yaotrue</a>
+     * 2017年5月25日 上午12:13:13
+     * @return
+     */
+    public synchronized static Jedis getJedis() {
+        try {
+            if (jedisPool != null) {
+                Jedis resource = jedisPool.getResource();
+                return resource;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    /**
+     * 释放jedis资源
+     * @author <a href="mailto:yaotrue@163.com">yaotrue</a>
+     * 2017年5月25日 上午12:13:04
+     * @param jedis
+     */
+    public static void returnResource(final Jedis jedis) {
+        if (jedis != null) {
+            jedis.close();
+        }
+    }
+}
